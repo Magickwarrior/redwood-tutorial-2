38a4a3b6cfa122b613fd7098b9b99dad29231880
Fernando Spitaliere <fernando.spitaliere@gmail.com>
2024-11-01 11:32:57 -0400

    Fixed Comments Functionality

--- api/src/services/comments/comments.test.js
+++ api/src/services/comments/comments.test.js
@@ -1,17 +1,19 @@
+import { db } from 'src/lib/db'
+
 import { comments, createComment } from './comments'
 
-// Generated boilerplate tests do not account for all circumstances
-// and can fail without adjustments, e.g. Float.
-//           Please refer to the RedwoodJS Testing Docs:
-//       https://redwoodjs.com/docs/testing#testing-services
-// https://redwoodjs.com/docs/testing#jest-expect-type-considerations
-
 describe('comments', () => {
-  scenario('returns all comments', async (scenario) => {
-    const result = await comments()
-
-    expect(result.length).toEqual(Object.keys(scenario.comment).length)
-  })
+  scenario(
+    'returns all comments for a single post from the database',
+    async (scenario) => {
+      const result = await comments({ postId: scenario.comment.jane.postId })
+      const post = await db.post.findUnique({
+        where: { id: scenario.comment.jane.postId },
+        include: { comments: true },
+      })
+      expect(result.length).toEqual(post.comments.length)
+    }
+  )
 
   scenario('postOnly', 'creates a new comment', async (scenario) => {
     const comment = await createComment({
